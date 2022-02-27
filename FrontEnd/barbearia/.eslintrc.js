4a2dcb806ad5d417fda8da5fc114ac278d8872a6
Tiago Pacheco <tiago@masterplanti.com.br>
2022-02-27 00:13:21 -0300

    Add ProjectVue

---
+++ FrontEnd/barbearia/.eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/typescript/recommended",
+    '@vue/prettier/@typescript-eslint'
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    indent: 'off',
+    'space-before-function-parent': 'off',
+    semi: 0,
+    'padded-blocks': 0,
+    'spaced-comment': 0,
+    'space-before-function-paren': [
+      'error',
+      {
+        anonymous: 'always',
+        named: 'never',
+        asyncArrow: 'always'
+      }
+    ]
+  },
+};
