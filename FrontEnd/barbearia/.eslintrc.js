7b5eb411ddbfff544a85eb55b1be7e11300577eb
Tiago Pacheco <tiago@masterplanti.com.br>
2022-02-27 00:59:50 -0300

    Start Default

--- FrontEnd/barbearia/.eslintrc.js
+++ FrontEnd/barbearia/.eslintrc.js
@@ -7,7 +7,7 @@ module.exports = {
     "plugin:vue/vue3-essential",
     "eslint:recommended",
     "@vue/typescript/recommended",
-    '@vue/prettier/@typescript-eslint'
+    "plugin:prettier/recommended",
   ],
   parserOptions: {
     ecmaVersion: 2020,
@@ -15,18 +15,5 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-    indent: 'off',
-    'space-before-function-parent': 'off',
-    semi: 0,
-    'padded-blocks': 0,
-    'spaced-comment': 0,
-    'space-before-function-paren': [
-      'error',
-      {
-        anonymous: 'always',
-        named: 'never',
-        asyncArrow: 'always'
-      }
-    ]
   },
 };
