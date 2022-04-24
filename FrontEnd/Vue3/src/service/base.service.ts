edcf5ecec34a527dd22861e232667b055eee4736
Tiago Pacheco <tiago@masterplanti.com.br>
2022-04-23 21:20:05 -0300

    Acept message erros fron back end

--- FrontEnd/Vue3/src/service/base.service.ts
+++ FrontEnd/Vue3/src/service/base.service.ts
@@ -1,6 +1,7 @@
 import services from './'
 import { useStore } from '@/store/store'
 import { EToastSeverity } from '@/Enums/ToastSeverity'
+
 export default (service: string) => {
   const baseUrl = 'api/v1/'
   const store = useStore()
@@ -8,13 +9,17 @@ export default (service: string) => {
   const getById = async (model) => {
     return await services().get(`${baseUrl + service}/${model.Id}`)
       .then((response) => {
-        console.log('getById response',response)
+        console.log('getById response', response)
         return response.data
       })
       .catch((error) => {
         console.log('Get: id enviada ->', model.Id)
         console.log('Erro get catch (error)', error)
-        store.prop.toastMessage = store.methods.toastMessage(EToastSeverity.Error, 'Falha!', error, 'main', 3000)
+        if (error.response.data.error) {
+          store.prop.toastMessage = store.methods.toastMessage(EToastSeverity.Error, 'Falha!', error.response.data.error, 'main', 3000)
+        } else {
+          store.prop.toastMessage = store.methods.toastMessage(EToastSeverity.Error, 'Falha!', error, 'main', 3000)
+        }
       })
   }
 
@@ -22,13 +27,17 @@ export default (service: string) => {
     return await services()
       .get(`${baseUrl + service}`)
       .then((response) => {
-        console.log('getAll response',response)
+        console.log('getAll response', response)
         return response.data
       })
       .catch((error) => {
         console.log('Get: ALL')
         console.log('Erro getALL catch (error)', error)
-        store.prop.toastMessage = store.methods.toastMessage(EToastSeverity.Error, 'Falha!', error, 'main', 3000)
+        if (error.response.data.error) {
+          store.prop.toastMessage = store.methods.toastMessage(EToastSeverity.Error, 'Falha!', error.response.data.error, 'main', 3000)
+        } else {
+          store.prop.toastMessage = store.methods.toastMessage(EToastSeverity.Error, 'Falha!', error, 'main', 3000)
+        }
       })
   }
 
@@ -36,16 +45,20 @@ export default (service: string) => {
     return await services()
       .post(`${baseUrl + service}`, model)
       .then((response) => {
-          console.log('SaveModel response',response)
-        if(response.status == 200){
+        console.log('SaveModel response', response)
+        if (response.status == 200) {
           store.prop.toastMessage = store.methods.toastMessage(EToastSeverity.Success, 'Sucesso!', 'Salvo em banco de dados', 'main', 2000)
         }
-          return response.data
-        })
+        return response.data
+      })
       .catch((error) => {
         console.log('Post: Model enviada ->', model)
         console.log('Erro Save catch (error)', error)
-        store.prop.toastMessage = store.methods.toastMessage(EToastSeverity.Error, 'Save', error, 'main', 3000)
+        if (error.response.data.error) {
+          store.prop.toastMessage = store.methods.toastMessage(EToastSeverity.Error, 'Falha!', error.response.data.error, 'main', 3000)
+        } else {
+          store.prop.toastMessage = store.methods.toastMessage(EToastSeverity.Error, 'Falha!', error, 'main', 3000)
+        }
       })
   }
 
@@ -53,12 +66,16 @@ export default (service: string) => {
     return await services()
       .post(`${baseUrl + service}`, model)
       .then((response) => {
-        console.log('login response',response)
+        console.log('login response', response)
         return response.data
       })
       .catch((error) => {
-        console.log('Login: Model enviada ->', model,error)
-        store.prop.toastMessage = store.methods.toastMessage(EToastSeverity.Error, 'Login', error, 'main', 3000)
+        console.log('Login: Model enviada ->', model, error)
+        if (error.response.data.error) {
+          store.prop.toastMessage = store.methods.toastMessage(EToastSeverity.Error, 'Falha!', error.response.data.error, 'main', 3000)
+        } else {
+          store.prop.toastMessage = store.methods.toastMessage(EToastSeverity.Error, 'Falha!', error, 'main', 3000)
+        }
       })
   }
 
@@ -66,15 +83,19 @@ export default (service: string) => {
     return await services()
       .post(`${baseUrl + service}/Cadastro`, model)
       .then((response) => {
-        console.log('newPerson response',response)
-        if(response.status != 200) {
+        console.log('newPerson response', response)
+        if (response.status != 200) {
           store.prop.toastMessage = store.methods.toastMessage(EToastSeverity.Info, 'Falha!', response.data.detail, 'main', 3000)
         }
         return response.data
       })
       .catch((error) => {
         console.log('NewLogin: Model enviada ->', model, error)
-        store.prop.toastMessage = store.methods.toastMessage(EToastSeverity.Error, 'Save', error, 'main', 3000)
+        if (error.response.data.error) {
+          store.prop.toastMessage = store.methods.toastMessage(EToastSeverity.Error, 'Falha!', error.response.data.error, 'main', 3000)
+        } else {
+          store.prop.toastMessage = store.methods.toastMessage(EToastSeverity.Error, 'Falha!', error, 'main', 3000)
+        }
       })
   }
 
@@ -82,16 +103,21 @@ export default (service: string) => {
     return await services()
       .put(`${baseUrl + service}`, model)
       .then((response) => {
-        console.log('editModel response',response)
-        if(response.status == 200){
+        console.log('editModel response', response)
+        if (response.status == 200) {
           store.prop.toastMessage = store.methods.toastMessage(EToastSeverity.Success, 'Sucesso!', 'Salvo em banco de dados', 'main', 2000)
         }
         return response.data
       })
       .catch((error) => {
         console.log('Put: Model enviada ->', model)
-        console.log('Erro Put catch (error)', error)
-        store.prop.toastMessage = store.methods.toastMessage(EToastSeverity.Error, 'Falha!', error, 'main', 3000)
+        console.log('Erro Put catch (error)', error.response.data.error)
+        console.log(error)
+        if (error.response.data.error) {
+          store.prop.toastMessage = store.methods.toastMessage(EToastSeverity.Error, 'Falha!', error.response.data.error, 'main', 3000)
+        } else {
+          store.prop.toastMessage = store.methods.toastMessage(EToastSeverity.Error, 'Falha!', error, 'main', 3000)
+        }
       })
   }
 
@@ -99,8 +125,8 @@ export default (service: string) => {
     return await services()
       .delete(`${baseUrl + service}/${model.Id}`)
       .then((response) => {
-        console.log('deleteModel response',response)
-        if(response.status == 200){
+        console.log('deleteModel response', response)
+        if (response.status == 200) {
           store.prop.toastMessage = store.methods.toastMessage(EToastSeverity.Info, 'Deletado com sucesso!', 'Excluido do banco de dados', 'main', 2000)
         }
         return response
@@ -108,7 +134,11 @@ export default (service: string) => {
       .catch((error) => {
         console.log('Delete: id enviado ->', model.Id)
         console.log('Erro Delete catch (error)', error)
-        store.prop.toastMessage = store.methods.toastMessage(EToastSeverity.Error, 'Falha!', error, 'main', 3000)
+        if (error.response.data.error) {
+          store.prop.toastMessage = store.methods.toastMessage(EToastSeverity.Error, 'Falha!', error.response.data.error, 'main', 3000)
+        } else {
+          store.prop.toastMessage = store.methods.toastMessage(EToastSeverity.Error, 'Falha!', error, 'main', 3000)
+        }
       })
   }
 
